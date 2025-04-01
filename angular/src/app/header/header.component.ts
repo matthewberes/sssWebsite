17ffb50f8ef9c065d668ff9095968e60d5b3022d
matthewberes <matthewberes@trentu.ca>
2025-04-01 19:30:29 -0400

    toolbar skeleton

--- angular/src/app/header/header.component.ts
+++ angular/src/app/header/header.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ToolbarComponent } from '../toolbar/toolbar.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [ToolbarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
