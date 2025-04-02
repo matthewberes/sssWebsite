633d19739cd27f5af74c6b7b3ec1978471396c24
matthewberes <mattyberes@gmail.com>
2025-04-02 15:54:21 -0400

    add maintenace request page

--- angular/src/app/main-content-form/main-content-form.component.ts
+++ angular/src/app/main-content-form/main-content-form.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
   selector: 'app-main-content-form',
-  imports: [],
+  imports: [MatRadioModule],
   templateUrl: './main-content-form.component.html',
   styleUrl: './main-content-form.component.css'
 })
