850c802d2cb966c8a01eaeccf4048a7e2f5a8a72
matthewberes <mattyberes@gmail.com>
2025-04-02 15:56:32 -0400

    add logo link to home

--- angular/src/app/header/header.component.ts
+++ angular/src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ToolbarComponent } from '../toolbar/toolbar.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +9,10 @@ import { ToolbarComponent } from '../toolbar/toolbar.component';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  constructor(private router: Router) { }
+
+  onClick() {
+    this.router.navigate(['/']);
+  }
 
 }
