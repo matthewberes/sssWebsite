f3f54928e7fb777f27eba49ef578d89206e2875c
matthewberes <matthewberes@trentu.ca>
2025-04-07 17:12:20 -0400

    add captcha

--- angular/src/app/main-content-form/main-content-form.component.ts
+++ angular/src/app/main-content-form/main-content-form.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatRadioModule } from '@angular/material/radio';
+import { RECAPTCHA_SETTINGS, RecaptchaFormsModule, RecaptchaModule, RecaptchaSettings } from 'ng-recaptcha';
+
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-main-content-form',
-  imports: [MatRadioModule],
+  imports: [MatRadioModule,
+    FormsModule,
+    RecaptchaModule,
+    RecaptchaFormsModule],
+  providers: [
+    {
+      provide: RECAPTCHA_SETTINGS,
+      useValue: {
+        siteKey: environment.recaptcha.siteKey,
+      } as RecaptchaSettings,
+    },
+  ],
   templateUrl: './main-content-form.component.html',
   styleUrl: './main-content-form.component.css'
 })
 export class MainContentFormComponent {
+
+  token: string | undefined;
+
+  constructor() {
+    this.token = undefined;
+  }
 
 }
